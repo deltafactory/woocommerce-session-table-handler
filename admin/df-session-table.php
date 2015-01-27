61732da0bc1a2b7ffcabd8053cf66e058003bf06
Jeff Brand <jeff@deltafactory.com>
2015-01-26 20:33:58 -0500

    Improved diagnostic page layout.

--- admin/df-session-table.php
+++ admin/df-session-table.php
@@ -30,6 +30,37 @@ $base_url = add_query_arg( 'page', $plugin_page, admin_url( 'admin.php' ) );
 $clear_all_sessions_url = wp_nonce_url( add_query_arg( 'action', 'clear_all', $base_url ), 'df-session-clear' );
 $clear_expired_sessions_url = wp_nonce_url( add_query_arg( 'action', 'clear_expired', $base_url ), 'df-session-clear' );
 ?>
+<style>
+
+.df-wc-session-table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+
+.df-wc-session-table th,
+.df-wc-session-table td {
+	border-collapse: collapse;
+	padding: 5px;
+}
+
+.df-wc-session-table td {
+	border: solid black 1px;
+}
+
+.df-wc-session-table tbody th {
+	text-align: right;
+}
+
+.df-wc-session-table td {
+	text-align: center;
+}
+
+.df-wc-session-table td.control {
+	text-align: left;
+	border: none;
+}
+
+</style>
 <div class="wrap">
 	<h2><?php echo $title ?></h2>
 
@@ -37,29 +68,37 @@ $clear_expired_sessions_url = wp_nonce_url( add_query_arg( 'action', 'clear_expi
 		<div class="updated"><p><?php echo $notice ?></p></div>
 	<?php } ?>
 
-	<table class="form-table">
-		<tr>
-			<th>Sessions</th>
-			<td>
-				<p>Total: <?php echo $all_sessions['total'] ?></p>
-				<p>User:  <?php echo $all_sessions['user'] ?></p>
-				<p>Guest: <?php echo $all_sessions['guest'] ?></p>
-
-				<p>
+	<table class="df-wc-session-table">
+		<thead>
+			<tr>
+				<th></th>
+				<th>Total</th>
+				<th>User</th>
+				<th>Guest</th>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th>All Sessions</th>
+				<td><?php echo $all_sessions['total'] ?></td>
+				<td><?php echo $all_sessions['user'] ?></td>
+				<td><?php echo $all_sessions['guest'] ?></td>
+				<td class="control">
 					<a class="button" href="<?php echo $clear_all_sessions_url ?>">Clear All Sessions</a>
 					<span class="description">Warning: This will empty carts of all active sessions</span>
-				</p>
-			</td>
-		</tr>
-
-		<tr>
-			<th>Expired Sessions</th>
-			<td>
-				<p>Total: <?php echo $expired_sessions['total'] ?></p>
-				<p>User:  <?php echo $expired_sessions['user'] ?></p>
-				<p>Guest: <?php echo $expired_sessions['guest'] ?></p>
-
-				<p><a class="button" href="<?php echo $clear_expired_sessions_url ?>">Clear Expired Sessions</a></p>
-			</td>
+				</td>
+			</tr>
+			<tr>
+				<th>Expired Sessions</th>
+				<td><?php echo $expired_sessions['total'] ?></td>
+				<td><?php echo $expired_sessions['user'] ?></td>
+				<td><?php echo $expired_sessions['guest'] ?></td>
+				<td class="control">
+					<a class="button" href="<?php echo $clear_expired_sessions_url ?>">Clear Expired Sessions</a>
+					<span class="description">These are automatically cleaned up twice a day.</span>
+				</td>
+			</tr>
+		</tbody>
 	</table>
 </div>
